e33325b9f6321d99cfa1dc240d9f95da63303fe7
usermine12 <mikaeljarv@gmail.com>
2022-03-21 22:34:11 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [index, setIndex] = useState(0)
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Radio-buttons</h1>
+        <div className="buttonGroup">
+          <button onClick={() => setIndex(0)} disabled={index === 0}>Val</button>
+          <button onClick={() => setIndex(1)} disabled={index === 1}>Val2</button>
+          <button onClick={() => setIndex(2)} disabled={index === 2}>Val3</button>
+          <button onClick={() => setIndex(3)} disabled={index === 3}>Val4</button>
+        </div>
+
+        <br></br>
+        <div>
+          Index: {index}
+        </div>
       </header>
     </div>
   );
